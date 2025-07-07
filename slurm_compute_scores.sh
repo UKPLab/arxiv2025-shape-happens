0abbcaac58d660af0d3cbbbd3d5096d34bba5d53
Federico Tiblias <tiblias@slurm-login.ukp.informatik.tu-darmstadt.de>
2025-07-07 18:51:16 +0200

    feat: update slurm scripts

---
+++ slurm_compute_scores.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --job-name=time_dynamics
+#SBATCH --output=/mnt/beegfs/work/tiblias/time-stuff/logs/compute_scores.txt
+#SBATCH --mail-user=federico.tiblias@tu-darmstadt.de
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH -q yolo
+#SBATCH -p yolo
+#SBATCH --mem=256GB
+
+
+# export PATH=$PATH:/ukp-storage-1/tiblias/miniconda/envs/td/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ukp-storage-1/tiblias/miniconda/envs/td/bin
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/ukp-storage-1/tiblias/miniconda/lib
+# export CUDA_VISIBLE_DEVICES="0"
+# export CC=/ukp-storage-1/tiblias/miniconda/envs/gcc/bin/gcc
+# export CXX=/ukp-storage-1/tiblias/miniconda/envs/gcc/bin/g++
+# export NCCL_P2P_LEVEL=NVL
+# export NCCL_IB_GID_INDEX=3
+# export NCCL_P2P_DISABLE=0
+# export NCCL_DEBUG=INFO
+cd /ukp-storage-1/tiblias/time-stuff
+source /ukp-storage-1/tiblias/miniconda/bin/activate td
+
+python compute_scores.py
